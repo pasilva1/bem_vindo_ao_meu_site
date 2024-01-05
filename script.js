a3097efb055ddaec37b9818f9fd227e593e6b032
Patrick Ataíde <43658340+pasilva1@users.noreply.github.com>
2024-01-05 12:28:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ class TypeWriter {
       this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
   
       // Initial Type Speed
-      let typeSpeed = 100;
+      let typeSpeed = 50;
   
       if(this.isDeleting) {
         typeSpeed /= 2;
